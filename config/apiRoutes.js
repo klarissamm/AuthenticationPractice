b47b0ca902a6e91e2e30336f458f9466eb5d7b36
klarissamm <klarissamunz@gmail.com>
2017-04-21 17:09:24 +0100

    added routes to edit andupdate in the backend

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -5,12 +5,16 @@ const authentications = require('../controllers/authentications');
 const users           = require('../controllers/users');
 
 router.route('/register')
-  .post(authentications.register);
+.post(authentications.register);
 router.route('/login')
-  .post(authentications.login);
+.post(authentications.login);
 
 router.route('/users')
-  .get(users.index);
+.get(users.index);
+router.route('/users/:id')
+.get(users.show)
+.put(users.update)
+.delete(users.delete);
 
 
 module.exports = router;
